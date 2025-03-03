60478e605eb1a3915af17fc326762548a873c89d
anarcher <anarcher@gmail.com>
2025-03-03 18:11:21 +0900

    feat(raindrop-io-mcp-server): Update Raindrop-io-mcp-server formula to v0.0.2

--- Formula/raindrop-io-mcp-server.rb
+++ Formula/raindrop-io-mcp-server.rb
@@ -1,13 +1,13 @@
 class RaindropIoMcpServer < Formula
   desc "Raindrop.io Media Control Protocol Server"
   homepage "https://github.com/anarcher/raindrop-io-mcp-server"
-  version "0.0.1"
+  version "0.0.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/anarcher/raindrop-io-mcp-server/releases/download/v0.0.1/raindrop-io-mcp-server_0.0.1_darwin_amd64.tar.gz"
-      sha256 "1c3d2753664cbf8108aa64ad0cbad20709d5ae9913aae3a9337046eef88beaae"
+      url "https://github.com/anarcher/raindrop-io-mcp-server/releases/download/v0.0.2/raindrop-io-mcp-server_0.0.2_darwin_amd64.tar.gz"
+      sha256 "8747c5c6f85caf5af649436cbc69c6d596b1d23c157bd54b18208594b0cab298"
 
       def install
         bin.install "raindrop-io-mcp-server"
@@ -15,8 +15,8 @@ class RaindropIoMcpServer < Formula
     end
 
     if Hardware::CPU.arm?
-      url "https://github.com/anarcher/raindrop-io-mcp-server/releases/download/v0.0.1/raindrop-io-mcp-server_0.0.1_darwin_arm64.tar.gz"
-      sha256 "da55fc96c6289e2217ae1ab99ceadbaaa2d59efca040f7e1e74a3df07bc2825b"
+      url "https://github.com/anarcher/raindrop-io-mcp-server/releases/download/v0.0.2/raindrop-io-mcp-server_0.0.2_darwin_arm64.tar.gz"
+      sha256 "4edefba89d314a6c59e2d6bf028bbea97ffa8f00a9f901f629e55801068c81fa"
 
       def install
         bin.install "raindrop-io-mcp-server"
